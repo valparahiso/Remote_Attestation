5c7fa9d8770fe2e1a8d5f00e41ab88ac5b0f83ac
valparahiso <simovuillermoz@gmail.com>
2022-03-07 17:43:21 +0100

    Created server and client folders, rewritten the start.sh files and the CMakes files

---
+++ client/source.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export LIBSODIUM_CLIENT_DIR=$(pwd)/libsodium_builds/libsodium_client/src/libsodium
--- source.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export LIBSODIUM_CLIENT_DIR=$(pwd)/libsodium_builds/libsodium_client/src/libsodium
-export LIBSODIUM_DIR=$(pwd)/libsodium_builds/libsodium_server/src/libsodium
