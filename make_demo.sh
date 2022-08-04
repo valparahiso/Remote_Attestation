68b50a292139e858a4477ce97340c2fd36f80b8d
valparahiso <simovuillermoz@gmail.com>
2022-08-04 10:01:54 +0200

    Sistemati i nomi corretti, ora tutto sembra funzionare, riparto da qui per tls

--- make_demo.sh
+++ make_demo.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 
-echo "1. Compilo il verifier"
+read -p "1. Compilo il verifier . . . Press any key to continue . . . " -n1 -s
 mkdir -p verifier/build
 cd verifier/trusted_verifier
 make
 
-echo "2. Compilo l'attester"
+read -p "2. Compilo l'attester . . . Press any key to continue . . . " -n1 -s
 cd ../../attester
 ./start_server.sh
 
-echo "3. Copio il file generato in qemu"
+read -p "3. Copio il file generato in qemu . . . Press any key to continue . . " -n1 -s
 cp build/demo-attester.ke ../../keystone_edit/build/overlay/root
 
-echo "4. Compilo il nuovo file in qemu"
+echo ". "
+
+read -p "4. Compilo il nuovo file in qemu . . . Press any key to continue . . . " -n1 -s
+rm -r ../../keystone_edit/build/buildroot.build/target/root/*
 cd ../../keystone_edit/build 
 make image
 
